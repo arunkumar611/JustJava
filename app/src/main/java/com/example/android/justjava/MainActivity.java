08849f376c0564153344e4a029350fa360ec0acb
killerapp7 <killerapp7@gmail.com>
2016-12-18 10:03:24 +0530

    After Name input

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,12 +5,16 @@
 package com.example.android.justjava;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -29,7 +33,17 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        quantity = quantity + 1;
+        if (quantity >= 100) {
+            Context context = getApplicationContext();
+            CharSequence text = "Quantity cannot go above 100";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        } else {
+            quantity = quantity + 1;
+        }
         displayQuantity(quantity);
     }
 
@@ -37,7 +51,17 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        quantity = quantity - 1;
+        if (quantity <= 1) {
+            Context context = getApplicationContext();
+            CharSequence text = "Quantity cannot go below 1";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        } else {
+            quantity = quantity - 1;
+        }
         displayQuantity(quantity);
     }
 
@@ -45,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+
+
         // Figure out if the user wants whipped cream topping
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
@@ -54,11 +80,18 @@ public class MainActivity extends AppCompatActivity {
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
         // Calculate the price
-        int price = calculatePrice();
+        int price = calculatePrice(quantity, hasWhippedCream, hasChocolate);
+
+        EditText customerName = (EditText) findViewById(R.id.customerName);
+        String name = customerName.getText().toString();
 
         // Display the order summary on the screen
-        String message = createOrderSummary(price, hasWhippedCream, hasChocolate);
-        displayMessage(message);
+        String message = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
+//        displayMessage(message);
+
+        String emailSubject = "JustJava order for " + name;
+        composeEmail(emailSubject, message);
+
     }
 
     /**
@@ -66,8 +99,15 @@ public class MainActivity extends AppCompatActivity {
      *
      * @return total price
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(int quantity, Boolean hasWhippedCream, Boolean hasChocolate) {
+        int price = 5;
+        if (hasWhippedCream) {
+            price += 1;
+        }
+        if (hasChocolate) {
+            price += 2;
+        }
+        return price * quantity;
     }
 
     /**
@@ -76,13 +116,12 @@ public class MainActivity extends AppCompatActivity {
      * @param price           of the order
      * @param addWhippedCream is whether or not to add whipped cream to the coffee
      * @param addChocolate    is whether or not to add chocolate to the coffee
+     * @param name            name of the customer
      * @return text summary
      */
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
-        EditText customerName = (EditText) findViewById(R.id.customerName);
-        String priceMessage = customerName.getText().toString();
-
-        //String priceMessage = "Name: Lyla the Labyrinth";
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String name) {
+        String priceMessage = "Name: ";
+        priceMessage += name;
         priceMessage += "\nAdd whipped cream? " + addWhippedCream;
         priceMessage += "\nAdd chocolate? " + addChocolate;
         priceMessage += "\nQuantity: " + quantity;
@@ -103,9 +142,17 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+//    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
+    public void composeEmail(String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
-
 }
